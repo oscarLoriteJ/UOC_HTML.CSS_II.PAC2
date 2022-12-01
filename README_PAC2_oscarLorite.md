1405915e364abf60d42e533a5ba04aa3510d0efa
Oscar Lorite <oscar.lorite@hotmail.com>
2022-12-01 01:23:09 +0100

    Punt de partida PAC2. Afegit Bootstrap com a dependència externa

---
+++ README_PAC2_oscarLorite.md
@@ -0,0 +1,11 @@
+# PAC1
+
+Remarques addicionals del projecte PAC2.
+
+**Autor**: Óscar Lorite Jódar
+
+## URL name of site deployment
+
+https://pac2-olj-htmlcssii.netlify.app/
+
+## Imatges utilitzades
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,31 +2,32 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
+import * as bootstrap from "bootstrap";
 import { LoremIpsum } from "lorem-ipsum";
 /**
  * Write any other JavaScript below
  */
+bootstrap +
+  (function () {
+    const university = "UOC";
+    console.log(`Hello, ${university}!`);
 
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4,
+      },
+    });
 
-  const lorem = new LoremIpsum({
-    sentencesPerParagraph: {
-      max: 8,
-      min: 4,
-    },
-    wordsPerSentence: {
-      max: 16,
-      min: 4,
-    },
-  });
-
-  document.getElementById("lorem-title").innerHTML = lorem.generateWords(5);
-  document.getElementById("lorem-title-mesInfo").innerHTML =
-    lorem.generateWords(8);
-  document.getElementById("lorem-titol-contingut").innerHTML =
-    lorem.generateWords(4);
-  document.getElementById("lorem-contingut").innerHTML =
-    lorem.generateParagraphs(1);
-})();
+    document.getElementById("lorem-title").innerHTML = lorem.generateWords(5);
+    document.getElementById("lorem-title-mesInfo").innerHTML =
+      lorem.generateWords(8);
+    document.getElementById("lorem-titol-contingut").innerHTML =
+      lorem.generateWords(4);
+    document.getElementById("lorem-contingut").innerHTML =
+      lorem.generateParagraphs(1);
+  })();
